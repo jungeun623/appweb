9f314fb3de368aee9e43ca82e9bf67e0a8f70544
jungeun623 <jungeun623@gmail.com>
2024-07-12 15:35:12 +0900

    반응형 적용

---
+++ js/main.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+    const swPromotion = new Swiper(".sw-promotion", {
+       
+        // navigation: {
+        //   nextEl: ".promotion .sw-next",
+        //   prevEl: ".promotion .sw-prev",
+        // },
+        loop: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+      });
+};
