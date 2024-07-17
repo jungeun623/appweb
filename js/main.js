ff14c62479c8311a54681345d725b03d95fbe98d
jungeun623 <jungeun623@gmail.com>
2024-07-17 18:10:40 +0900

    파비콘 작업, 반응형 작업

--- js/main.js
+++ js/main.js
@@ -1,34 +1,48 @@
 window.onload = function () {
-    const swPromotion = new Swiper(".sw-promotion", {
-       
-        // swiper ===========
-        loop: true,
-        autoplay: {
-          delay: 2500,
-          disableOnInteraction: false,
-        },
-        grid: {
-          rows: 1,
-          fill: "row",
-        },
-        pagination: {
-          el: ".sw-promotion-pg ",
-          clickable: true,
-        },
-        // breakpoints: {
-        //   480: {
-        //     spaceBetween: 32,
-        //     slidesPerView: 2,
-        //     slidesPerGroup: 2,
-        //     grid: {
-        //       rows: 2,
-        //     },
-        //   },
-        //   1280: {
-        //     spaceBetween: 26,
-        //     slidesPerView: 1,
-        //     // slidesPerGroup: 3,
-        //   },
-        // },
-      });
+  const swPromotion = new Swiper(".sw-promotion", {
+    // swiper ===========
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    grid: {
+      rows: 1,
+      fill: "row",
+    },
+    pagination: {
+      el: ".sw-promotion-pg ",
+      clickable: true,
+    },
+  });
+  const swMeal = new Swiper(".sw-medal", {
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+   
+    breakpoints: {
+      480:{
+        slidesPerView: 2,
+        spaceBetween: 30,
+      }
+    },
+  });
 };
+// breakpoints: {
+//   480: {
+//     spaceBetween: 32,
+//     slidesPerView: 2,
+//     slidesPerGroup: 2,
+//     grid: {
+//       rows: 2,
+//     },
+//   },
+//   1280: {
+//     spaceBetween: 26,
+//     slidesPerView: 1,
+//     // slidesPerGroup: 3,
+//   },
+// },
