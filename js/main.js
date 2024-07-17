f9cecf2451f103574e7eb44d62e2da4b8e7b8a7b
jungeun623 <jungeun623@gmail.com>
2024-07-17 09:29:05 +0900

    하단부분 수정

--- js/main.js
+++ js/main.js
@@ -1,14 +1,34 @@
 window.onload = function () {
     const swPromotion = new Swiper(".sw-promotion", {
        
-        // navigation: {
-        //   nextEl: ".promotion .sw-next",
-        //   prevEl: ".promotion .sw-prev",
-        // },
+        // swiper ===========
         loop: true,
         autoplay: {
           delay: 2500,
           disableOnInteraction: false,
         },
+        grid: {
+          rows: 1,
+          fill: "row",
+        },
+        pagination: {
+          el: ".sw-promotion-pg ",
+          clickable: true,
+        },
+        // breakpoints: {
+        //   480: {
+        //     spaceBetween: 32,
+        //     slidesPerView: 2,
+        //     slidesPerGroup: 2,
+        //     grid: {
+        //       rows: 2,
+        //     },
+        //   },
+        //   1280: {
+        //     spaceBetween: 26,
+        //     slidesPerView: 1,
+        //     // slidesPerGroup: 3,
+        //   },
+        // },
       });
 };
